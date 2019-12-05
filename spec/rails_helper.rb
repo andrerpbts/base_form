a1c4e71e9483822204119a927b9c2db5c2812ac4
André Rodrigues <andrerpbts@gmail.com>
2019-12-05 13:26:22 -0200

    Adjust spec helpers files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 require 'rails/all'
 require 'rspec/rails'
 require 'support/test_app/config/environment'
+
+require 'support/factory_bot'
+require 'support/shoulda'
+require 'base_form'
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -13,11 +19,4 @@ ActiveRecord::Migration.maintain_test_schema!
 ActiveRecord::Schema.verbose = false
 load 'support/test_app/db/schema.rb' # use db agnostic schema by default
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
 
-require 'spec_helper'
